7cbe182138b6bb8715929c949817eb752efd4f90
bekor <bekor@gmail.com>
2017-05-04 14:26:40 +0200

    Delete function in backend created, test needed

--- common.py
+++ common.py
@@ -1,7 +1,11 @@
 import data_manager
 
 
-# ID;Submisson Time;View Number;Vote Number;Title;Message;Image
+# Create new question with its elements in the csv
+# CSV title: ID;Submisson Time;View Number;Vote Number;Title;Message;Image
+#
+# @data: list of list - whole answer table 
+# @req_form: dictionary from html form 
 def get_new_question(data, req_form):
     idx = str(int(data[-1][0]) + 1)
     local_time = data_manager.get_unixtime()
@@ -12,21 +16,29 @@ def get_new_question(data, req_form):
     image = ""
     new_question = [idx, local_time, view_number, vote_number,
                     title, data_form_story, image]
-    print(new_question)
     data_manager.append_datatable_to_file("/data/question.csv", new_question, (4, 5, 6))
     return new_question
 
 
-# ID;Submisson Time;Vote Number;Question ID;Message;Image
+# Create new answer with its elements in the csv
+# CSV title: ID;Submisson Time;Vote Number;Question ID;Message;Image
+#
+# @data: list of list - whole answer table
+# @req_form: dictionary from html form
+# @question_id: int - index of the question
 def get_new_answer(data, req_form, question_id):
     idx = str(int(data[-1][0]) + 1)
     local_time = data_manager.get_unixtime()
     view_number = "0"
-    question_id = question_id
     message = req_form["answer"]
     image = ""
     new_answer = [idx, local_time, view_number, question_id,
                   message, image]
     data_manager.append_datatable_to_file("/data/answer.csv", new_answer, (4, 5))
-    print(new_answer)
     return data
+
+
+def delete_data_by_id(data, button_value, code_columns, question_id):
+    new_data = [row for row in data if row[question_id] != button_value]
+    data_manager.write_datatable_to_file("data/story.csv", new_data, code_columns)
+    return new_data
